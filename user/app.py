c872fbdb55b9ff168df6bc0d01d94c042ddcec58
mohith <mohithb987@gmail.com>
2024-08-25 10:06:53 -0400

    initial setup - user side

---
+++ user/app.py
@@ -0,0 +1,13 @@
+import boto3
+import streamlit
+import uuid
+import os
+
+
+def main():
+    streamlit.header("GenAI-PDFInteraction App")
+    streamlit.write("Hi, welcome to the homepage.")
+
+
+if __name__ == "__main__":
+    main()
